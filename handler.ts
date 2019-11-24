eb61b553334a0b1a16a26b96e8396db0f9acbfa2
Igor Soroka <stupbird@gmail.com>
2019-11-24 14:32:41 +0200

    more stuff related to upload

--- handler.ts
+++ handler.ts
@@ -6,23 +6,18 @@ interface HelloResponse {
   body: string;
 }
 
+const bot = new Telegraf(process.env.BOT_TOKEN!);
+
 const sendAnswer: Handler = async (event: any, context: Context, callback: Callback) => {
-  //https://api.telegram.org/bot<bot_token>/getFile?file_id=the_file_id
-  //https://api.telegram.org/botTOKEN/getFile?file_id=FILE_ID
-  //Response: {"ok":true,"result":{"file_id":"the_file_id","file_size":50729,"file_path":"PATH"}}
-  //Download: https://api.telegram.org/file/bot1002584568:AAF55DOniBlgTmy7sFhfsZpSbP-7kxcbr2g/PATH
   const body = JSON.parse(event.body);
   const userInput = body.message.text;
   const chatId = body.message.chat.id;
-  const bot = new Telegraf(process.env.BOT_TOKEN!);
 
   if (body.message.document) {
-    const documentData = body.message.document;
-    if (documentData.mime_type === "application/pdf") {
-      bot.telegram.sendMessage(chatId, `File accepted`);
-    } else {
-      bot.telegram.sendMessage(chatId, `File is not accepted! Please send file in PDF format`);
-    }
+    const documentData = <DocumentData>body.message.document;
+    const answerForPdfCheck = answerIfPdfFormat(documentData);
+    copyFileToS3(documentData);
+    bot.telegram.sendMessage(chatId, answerForPdfCheck);
   } else {
     bot.on("text", ctx => ctx.reply(`You said: ${userInput} and your chatId: ${chatId}`));
     console.log("Request: " + JSON.stringify(body));
@@ -35,6 +30,28 @@ const sendAnswer: Handler = async (event: any, context: Context, callback: Callb
   };
 
   callback(null, response);
+};
+
+const answerIfPdfFormat = (documentData: DocumentData): string => {
+  if (documentData.mime_type === "application/pdf") {
+    return `File accepted`;
+  } else {
+    return `File is not accepted! Please send file in PDF format`;
+  }
+};
+
+const copyFileToS3 = (documentData: DocumentData): void => {
+  //https://api.telegram.org/bot<bot_token>/getFile?file_id=the_file_id
+  //https://api.telegram.org/bot<TOKEN>/getFile?file_id=FILE_ID
+  //Response: {"ok":true,"result":{"file_id":"the_file_id","file_size":50729,"file_path":"PATH"}}
+  //Download: https://api.telegram.org/file/bot<TOKEN>/PATH
+};
+
+type DocumentData = {
+  file_name: string;
+  mime_type: string;
+  file_id: string;
+  file_size: number;
 };
 
 export { sendAnswer };
