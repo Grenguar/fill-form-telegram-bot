47f4a088dc79e7476bd3cb519e0877a81d9aa492
Igor <stupbird@gmail.com>
2019-12-04 09:34:20 +0200

    Update questions.ts

--- src/questions.ts
+++ src/questions.ts
@@ -3,9 +3,9 @@ import FormQuestion from "./model/formQuestion";
 export default class Questions {
   private _questions: FormQuestion[];
   private commonErrorString = "К сожалению, произошла ошибка.";
-  private _welcomeWords = "Привет! Меня зовут Иван Гиков. Я помогу тебе оставить заявку на консультацию в Гик Экспорт.";
-  private _finalWords = "Спасибо! Ваши ответы приняты.";
-  private _afterFinalWords = "Вы уже ответили на все наши вопросы. Спасибо еще раз!";
+  private _welcomeWords = "Привет! Меня зовут Иван Гиков. Я помогу тебе оставить заявку на консультацию в GeekExport.";
+  private _finalWords = "Спасибо! Твои ответы приняты. Мы свяжемся с тобой в течение 1 дня.";
+  private _afterFinalWords = "Ты уже ответил на все наши вопросы. Спасибо еще раз!";
   private _typeError = "Извините, но такой тип файлов не поддерживается.";
 
   constructor() {
@@ -13,7 +13,7 @@ export default class Questions {
       {
         shortName: ":n",
         name: "username",
-        text: "Как Вас Зовут?",
+        text: "Как я могу к тебе обращаться?",
         type: "name",
         num: 0,
         error: `${this.commonErrorString} Имя должно быть не длиннее 50 символов и содержать только буквы`
@@ -21,7 +21,7 @@ export default class Questions {
       {
         shortName: ":e",
         name: "email",
-        text: "Какой у Вас E-mail?",
+        text: "Сообщи , пожалуйста, твой E-mail.",
         type: "email",
         num: 1,
         error: `${this.commonErrorString} E-mail должен быть yourname@yourdomain.com`
@@ -29,39 +29,39 @@ export default class Questions {
       {
         shortName: ":p",
         name: "phone",
-        text: "Какой у Вас номер телефона?",
+        text: "Напиши, пожалуйста, твой номер телефона.",
         type: "phone",
         num: 2,
-        error: `${this.commonErrorString} Номер телефона должен содержать только цифры и +`
+        error: `${this.commonErrorString} Номер телефона должен содержать только цифры и +.`
       },
       {
         shortName: ":ed",
         name: "education",
-        text: "Какое у Вас образование?",
+        text: "Напиши, пожалуйста, какое у тебя образование и специальность (среднее, высшее, студент)",
         type: "longString",
         num: 3,
-        error: `${this.commonErrorString} Поле должно содержать не более 300 символов`
+        error: `${this.commonErrorString} Поле должно содержать не более 300 символов.`
       },
       {
         shortName: ":exp",
         name: "experience",
-        text: "Какой у Вас опыт работы?",
+        text: "Есть ли у тебя опыт работы в сфере IT? (разработка, тестирование, дизайн, игры, инфраструктура)",
         type: "longString",
         num: 4,
-        error: `${this.commonErrorString} Поле должно содержать не более 300 символов`
+        error: `${this.commonErrorString} Поле должно содержать не более 300 символов.`
       },
       {
         shortName: ":w",
         name: "why",
-        text: "Почему Вы хотите получить отзыв о Вашем резюме?",
+        text: "Какова для тебя цель получить резюме ревью?",
         type: "longString",
         num: 5,
-        error: `${this.commonErrorString} Поле должно содержать не более 300 символов`
+        error: `${this.commonErrorString} Поле должно содержать не более 300 символов.`
       },
       {
         shortName: ":u",
         name: "upload",
-        text: "Загрузите, пожалуйста, Ваше резюме.",
+        text: "С удовольствием помогу тебе. Загрузи, пожалуйста, свое резюме в формате pdf, doc, docx, odt, txt.",
         type: "file",
         num: 6,
         error: `${this.commonErrorString} Файл не принят. Проверьте расширение. Я принимаю: pdf, doc, docx, odt, txt. Также файлы размером не более 15 МБ`,
